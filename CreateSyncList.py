7ab066a0378b40cb8392321cf308619146b85f5a
Tommy Einarsson <tommy@einarsson.nu>
2017-01-08 13:30:47 +0100

    Added reading values from cfg-file. A new sync now starts from the first show in the current playlist or if it is empty, the show specified in the cfg-file. Number of shows to sync is also specified in the cfg-file
     ToDo: Write down the updated configuration.

--- CreateSyncList.py
+++ CreateSyncList.py
@@ -4,19 +4,50 @@ The playlist contains the first episode of each show in the TV section.
 Shows specified in ExludeShows are not added to the playlist.
 """
 from plexapi.server import PlexServer
+from plexapi.myplex import MyPlexAccount
+import ConfigParser
+
 # plex = PlexServer()
 
-from plexapi.myplex import MyPlexAccount
 
 baseurl = 'http://192.168.2.50:32400'
 token = 'zbv7hDnHEV2aGKRmQsRd'
 plex = PlexServer(baseurl, token)
+Config = ConfigParser.ConfigParser()
+playlistName = ''
 
-Playlist = plex.playlist('To Sync')
+Config.read('CreateSyncList.cfg')
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+playlistName = ConfigSectionMap('General')['playlistname']
+numberOfShows2Add = ConfigSectionMap('General')['numberofepisodes2sync']
+
+firstShowToAdd = ConfigSectionMap(playlistName)['firstsyncedshow']
+
+if playlistName == '':
+    print ("No playlist specified. Please add PlaylistName under [General] in CreateSyncList.cfg")
+    exit()
+
+
+Playlist = plex.playlist(playlistName)
 
 thefirstlist = [];
 AllEpisodes = [];
 TVEpisodes = 0;
+
+# Playlist = plex.createPlaylist('TestSync',thefirstlist)
 
 # Define the Shows to exclude from the sync list
 ExcludeShows = ['24', 'Agatha Raisin', 'The Americans', 'Arne Dahl', 'Better Call Saul', 'The Big Bang Theory',
@@ -34,8 +65,16 @@ ExcludeShows = ['24', 'Agatha Raisin', 'The Americans', 'Arne Dahl', 'Better Cal
                 'Scorpion', 'Scrubs', 'Sherlock', 'Suits', 'The Taste', 'The Taste UK', 'Top Chef', 'Westworld'];
 
 # First, clear the current laylist
+# Save the first items show for later usage
+showName = ''
 for episode in Playlist.items():
+    if(showName == ''):
+        showName = episode.grandparentTitle
     Playlist.removeItem(episode)
+
+# If we still have shows left in the current playlist, start from that show
+if(showName != ''):
+    firstShowToAdd = showName
 
 TVsection = plex.library.section('TV')
 for shows in TVsection.all():
@@ -44,16 +83,28 @@ for shows in TVsection.all():
         print('Excluded: ', shows.title)
         continue
 
+    if ((firstShowToAdd != '') and (firstShowToAdd != shows.title)):
+        continue
+    else:
+        firstShowToAdd = ''
+
     if(len(shows.episodes()) <= 3):
         for episode in shows.episodes():
             thefirstlist.append(episode)
+            TVEpisodes += 1
         AllEpisodes.append(shows.title)
     else:
         thefirstlist.append(shows.episodes()[0])
 
     TVEpisodes += 1;
 
+    if(TVEpisodes >= int(numberOfShows2Add)):
+        break
+
 Playlist.addItems(thefirstlist)
+
+Config.set(playlistName, thefirstlist[0].grandparentTitle)
+Config.set(playlistName, shows.title)
 
 print('Episodes added to the Playlist:', TVEpisodes)
 for show in AllEpisodes:
