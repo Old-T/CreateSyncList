60da8dc2e32011e87b7d5944986598203939e64a
Tommy Einarsson <tommy@einarsson.nu>
2017-01-13 12:04:52 +0100

    Cleaning up. Move ExcludeList to cfg-file

--- CreateSyncList.py
+++ CreateSyncList.py
@@ -35,6 +35,10 @@ playlistName = ConfigSectionMap('General')['playlistname']
 numberOfShows2Add = ConfigSectionMap('General')['numberofepisodes2sync']
 
 startAfterThisShow = ConfigSectionMap(playlistName)['lastsyncedshow']
+doNotSyncThese = ConfigSectionMap(playlistName)['excludeshows']
+doNotSyncThese = doNotSyncThese.replace('\n','') # Remove the \n from the read string
+
+ExcludeShows = doNotSyncThese.split(",")
 
 if playlistName == '':
     print ("No playlist specified. Please add PlaylistName under [General] in CreateSyncList.cfg")
@@ -47,26 +51,8 @@ thefirstlist = [];
 AllEpisodes = [];
 TVEpisodes = 0;
 
-# Define the Shows to exclude from the sync list
-ExcludeShows = ['24', 'Agatha Raisin', 'The Americans', 'Arne Dahl', 'Better Call Saul', 'The Big Bang Theory',
-                'The Blacklist', 'The Blacklist: Redemption',
-                'Blackadder', 'Castle (2009)', 'Blindspot', 'Bones', 'Bosch', 'Broadchurch', 'Criminal Minds',
-                'Criminal Minds: Beyond Borders',
-                'Death in Paradise', 'Elementary', 'Fortitude', 'Friends', 'Game of Thrones', 'The Goose Mother',
-                'Hawaii Five-0', 'Homeland',
-                'Hooten & the Lady', 'House of Cards (US)', 'How I Met Your Mother', 'The Last Man on Earth',
-                'Lethal Weapon', 'Lucifer',
-                'Making a Murderer', 'Mars (2016)', 'MasterChef', 'MasterChef (US)', 'MasterChef Australia',
-                'MasterChef Canada', 'MasterChef South Africa',
-                'MasterChef: The Professionals', 'Midnight Sun', 'NCIS', 'NCIS: Los Angeles', 'NCIS: New Orleans',
-                'Orange Is the New Black', 'Orphan Black',
-                'Scorpion', 'Scrubs', 'Sherlock', 'Suits', 'The Taste', 'The Taste UK', 'Top Chef', 'Westworld'];
-
-# First, clear the current laylist
-# Save the first items show for later usage
 showName = ''
 StartFromCurrent = False
-StartAfterLast = False
 
 # Check for the last show in the playlist and start adding episodes after that.
 # We don't want to  transcode and sync those again
@@ -74,45 +60,37 @@ for episode in Playlist.items():
     showName = episode.grandparentTitle
     StartFromCurrent = True
     TVEpisodes += 1
-#    Playlist.removeItem(episode)
 
-# answer = input("Sync the device to clear all episodes! Press enter when ready.")
-
+alreadyInList = TVEpisodes
 # If we still have shows left in the current playlist, start from that show
 if(showName != ''):
     firstShowToAdd = showName
-    StartAfterLast = True
-
-BreakOnNext = False
 
 TVsection = plex.library.section('TV')
 for shows in TVsection.all():
-    # print(shows.title)
+
     if(StartFromCurrent):
-        if(firstShowToAdd != shows.title):
-            continue
-        else:
+        if(firstShowToAdd == shows.title):
             StartFromCurrent = False
-            continue    # Start adding next show
+        continue
 
     if ExcludeShows.count(shows.title) != 0:
         print('Excluded: ', shows.title)
         continue
 
-
-#    if(StartAfterLast):
-#        if(startAfterThisShow != shows.title):
-#            continue
-#        else:
-#            StartAfterLast = False
-
     if(len(shows.episodes()) <= 3):
         for episode in shows.episodes():
             thefirstlist.append(episode)
+            print ("Show: {} - S{}E{} - {}".format(episode.grandparentTitle,
+                                                          episode.seasonNumber,
+                                                          episode.index,
+                                                          episode.title))
             TVEpisodes += 1
         AllEpisodes.append(shows.title)
     else:
         thefirstlist.append(shows.episodes()[0])
+        print ("Show: {} - S{}E{} - {}".format(shows.episodes()[0].grandparentTitle, shows.episodes()[0].seasonNumber,
+                shows.episodes()[0].index, shows.episodes()[0].title))
 
     TVEpisodes += 1;
 
@@ -128,6 +106,6 @@ Config.set(playlistName, 'LastSyncedShow', shows.title)
 with open(r'CreateSyncList.cfg', 'wb') as configfile:
     Config.write(configfile)
 
-print('Episodes added to the Playlist:', TVEpisodes)
+print('Episodes added to the Playlist:', TVEpisodes - alreadyInList)
 for show in AllEpisodes:
     print( 'All episodes are added for show: ', show)
